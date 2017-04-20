c9e203e5ae1840c0af176cd36f43d5deaf9d88f8
Uchio KONDO <udzura@pepabo.com>
2017-04-20 11:18:22 +0900

    Add smoke test

--- test/dynaloader_test.rb
+++ test/dynaloader_test.rb
@@ -2,6 +2,12 @@
 ## DynaLoader Test
 ##
 
-assert("DynaLoader.new") do
+assert("DynaLoader") do
   assert_true DynaLoader.is_a?(Class)
+end
+
+assert("DynaLoader.new") do
+  t = DynaLoader.new("libc.so.6")
+  assert_equal DynaLoader, t.class
+  assert_equal String, t.handler_ptr_str.class
 end
